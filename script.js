b7a3685ef63ea9560e2fef26827b303d9573fdde
Ayush Singh <ayush5292singh@gmail.com>
2026-07-20 16:06:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -52,3 +52,48 @@ function deleteTask(index){
 
 
 showTasks();
+let time = 1500;
+let timer;
+
+
+function startTimer(){
+
+    clearInterval(timer);
+
+    timer = setInterval(function(){
+
+        let minutes = Math.floor(time / 60);
+        let seconds = time % 60;
+
+        document.getElementById("timer").innerHTML =
+        minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+
+
+        time--;
+
+
+        if(time < 0){
+
+            clearInterval(timer);
+
+            alert("🎉 Study session completed!");
+
+            time = 1500;
+
+        }
+
+    },1000);
+
+}
+
+
+
+function resetTimer(){
+
+    clearInterval(timer);
+
+    time = 1500;
+
+    document.getElementById("timer").innerHTML = "25:00";
+
+}
